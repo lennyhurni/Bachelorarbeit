9eaa82878f3028bb3d844540cc18fd8dbd685a95
lennyhurni <lenny.hurni@students.bfh.ch>
2025-03-25 16:12:28 +0100

    Add Docker support with Dockerfile and .dockerignore; update next.config.ts for standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
